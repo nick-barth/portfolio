80173a723d32ebedac47a100c19acc70a020bcb1
nick <barth.nicholas@gmail.com>
2019-05-15 22:03:50 +0200

    added selected workerinos

--- src/components/project/index.js
+++ src/components/project/index.js
@@ -1,11 +1,19 @@
 import React from "react"
+import Img from "gatsby-image"
+import Seperator from "../seperator"
 
-export default ({ title, description, img }) => {
+export default ({ title, description, img, location }) => {
   return (
     <div className="project">
+      <div className="project__background" />
       <div className="project__info">
         <div className="project__title">{title}</div>
+        <div className="project__location">{location}</div>
+        <Seperator />
         <div className="project__description">{description}</div>
+      </div>
+      <div className="project__image">
+        <Img fluid={img} />
       </div>
     </div>
   )
--- src/pages/work.js
+++ src/pages/work.js
@@ -1,19 +1,78 @@
 import React from "react"
+import { graphql } from "gatsby"
+import Project from "../components/project"
 
 import Seperator from "../components/seperator"
 
-export default () => {
+export default props => {
+  const projects = [
+    {
+      title: "CentralApp",
+      description: "Online presense solutions for brick and mortor retail",
+      location: "Brussels",
+      img: props.data.centralApp.childImageSharp.fluid,
+    },
+    {
+      title: "Deedmob",
+      description: "Empowering volunteers and volunteer organisations",
+      location: "Amsterdam",
+      img: props.data.centralApp.childImageSharp.fluid,
+    },
+    {
+      title: "Code Kingdoms",
+      description: "Teaching kids to code via minecraft",
+      location: "London",
+      img: props.data.centralApp.childImageSharp.fluid,
+    },
+    {
+      title: "Nxchange",
+      description: "A fully regulated stock exchange as a service",
+      location: "Brussels",
+      img: props.data.centralApp.childImageSharp.fluid,
+    },
+    {
+      title: "X Counters",
+      description:
+        "A suite of eight websites for promoting video game infrastructure",
+      location: "Earth",
+      img: props.data.centralApp.childImageSharp.fluid,
+    },
+    {
+      title: "Poop Log",
+      description: "errr... its a poop log",
+      location: "Buffalo",
+      img: props.data.centralApp.childImageSharp.fluid,
+    },
+  ]
+
   return (
     <div className="work">
       <div className="work__content">
         <div className="work__header">Selected Work</div>
         <Seperator />
-        <div className="work__projects">
+        <div className="work__description">
           SPAs, templating engines, static sites, static site generators,
           complex dashboards, data visualisations, CMS, servers, APIs,
           databases, I've done it all, and I'd do it all again.
         </div>
       </div>
+      <div className="work__projects">
+        {projects.map(project => (
+          <Project key={project.title} {...project} />
+        ))}
+      </div>
     </div>
   )
 }
+
+export const query = graphql`
+  query {
+    centralApp: file(relativePath: { eq: "centralapp.png" }) {
+      childImageSharp {
+        fluid(maxWidth: 1000) {
+          ...GatsbyImageSharpFluid
+        }
+      }
+    }
+  }
+`
