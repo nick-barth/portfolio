a44de2e440576d4198f14bb25ee496a77f4495a1
nick <barth.nicholas@gmail.com>
2019-05-17 12:59:24 +0200

    added some likes and dislikes

--- src/pages/personal.js
+++ src/pages/personal.js
@@ -1,9 +1,86 @@
 import React from "react"
 
-export default () => {
+import Seperator from "../components/seperator"
+
+export default props => {
   return (
     <div className="personal">
-      <div>Personal</div>
+      <div className="personal__content">
+        <div className="personal__header">Personal</div>
+        <Seperator />
+        <div className="personal__description">
+          As we all do, I have a intricite and full life outside of productivity
+          and capital generation. Here is a brief overview.
+        </div>
+        <div className="personal__container">
+          <div className="personal__category">Hobbies</div>
+          <ul className="personal__personal">
+            <li>
+              <a href="https://cmoreira.net/visited-countries-map/?map=5cde8fc2e1370">
+                Travel
+              </a>
+            </li>
+            <li>Squash</li>
+            <li>
+              <a href="https://www.goodreads.com/user/show/60480445-nick-barth">
+                Reading
+              </a>
+            </li>
+            <li>Gym</li>
+            <li>Cooking</li>
+            <li>
+              <a href="https://www.hackyourfuture.net/">Volunteering</a>
+            </li>
+          </ul>
+        </div>
+        <div className="personal__container">
+          <div className="personal__category">Passions</div>
+          <ul className="personal__personal">
+            <li>People</li>
+            <li>
+              <a href="https://open.spotify.com/user/1215590034?si=NX0c4GtASiGR5SecTg1LTQ">
+                Music
+              </a>
+            </li>
+            <li>Social causes</li>
+            <li>Being well rounded</li>
+            <li>
+              <a
+                target="_blank"
+                href="https://www.youtube.com/watch?v=npyJ0lECjv0"
+              >
+                Lovely strolls through gardens
+              </a>
+            </li>
+          </ul>
+        </div>
+        <div className="personal__container">
+          <div className="personal__category">Things I like</div>
+          <ul className="personal__personal">
+            <li>Laughing</li>
+            <li>Public Transport</li>
+            <li>
+              <a href="https://www.reddit.com/user/nickinkorea">Reddit</a>
+            </li>
+            <li>Working in cafes</li>
+            <li>Sunshine</li>
+            <li>Standardization in engineering</li>
+            <li>Indiviualism in people</li>
+          </ul>
+        </div>
+        <div className="personal__container">
+          <div className="personal__category">Things I dislike</div>
+          <ul className="personal__personal">
+            <li>Dependencies</li>
+            <li>Injustice</li>
+            <li>True crime documentaries</li>
+            <li>
+              People telling me to watch tv shows, I really can't invest 40
+              hours into this show, I'm sorry.
+            </li>
+          </ul>
+        </div>
+      </div>
     </div>
   )
 }
